17f0e609ec380f84889b240a5c4c69ab1ea2de08
dattebayo <fkgame061@gmail.com>
2025-08-26 11:39:15 +0500

    +msgbox

--- ricky.c
+++ ricky.c
@@ -2,5 +2,8 @@
 
 int main(){
     ShellExecute(0, "open", "https://www.youtube.com/watch?v=dQw4w9WgXcQ", 0, 0, SW_SHOWNORMAL);
+    Sleep(4000);
+    MessageBoxW(NULL, L"Get a better PC peasant 💀", L"Bruh", MB_OK | MB_TOPMOST | MB_SYSTEMMODAL | MB_ICONINFORMATION);
     return 0;
 }
+
